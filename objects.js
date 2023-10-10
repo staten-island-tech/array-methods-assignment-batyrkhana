217246e0f37c006d04bbc351d6ad413a3cd971fb
batyrkhana <batyrkhansa@gmail.com>
2023-10-10 09:27:43 -0400

    d1

--- objects.js
+++ objects.js
@@ -1,12 +1,47 @@
-let students = [
+const NFLPlayers = [
     {
-    firstName: 'Jane',
-    lastName: 'LaDooka',
-    graduated: false,
-    DOB: 2008,
-    }
+        firstName: "Mike",
+        lastName: "Evans",
+        age: 30,
+        Number: 13,
+        Position: "Receiver",
+        Team: "Tampa Bay",
+        Abilities: [Catch, Run, Jump],
+    },
+    {
+        firstName: "Tom",
+        lastName: "Brady",
+        age: 46,
+        Number: 12,
+        Position: "Quarterback",
+        Team: "Tampa Bay",
+        Abilities: [Throw, Run],
+    },
+    {
+        firstName: "Nick",
+        lastName: "Chubb",
+        age: 27,
+        Number: 24,
+        Position: "Runningback",
+        Team: "Cleveland Browns",
+    },
+    {
+        firstName: "Justin",
+        lastName: "Tucker",
+        age: 33,
+        Number: 9,
+        Position: "Kicker",
+        Team: "Baltimore Ravens",
+        Abilities: [Kick],
+    },
+    {
+        firstName: "Oddel",
+        lastName: "Beckham",
+        age: 30,
+        Number: 3,
+        Position: "Receiver",
+        Team: "Baltimore Ravens",
+    },
 ]
 
-students.forEach((student)=>console.log(student.firstName))
-const graduates = students.filter((student)=>student.graduated === true);
-console.log(graduates)
+NFLPlayers.forEach((player)=>console.log(player.firstName))
