9908ba94b046088ad20989c8d8cfc091c40982fa
batyrkhana <batyrkhansa@gmail.com>
2023-11-03 09:24:40 -0400

    p2

---
+++ problem2.js
@@ -0,0 +1,13 @@
+function fibbonaci(integer) {
+    let currentNum = 1;
+    let previousNum = 0; 
+    console.log(previousNum)
+    for (let i=2; i <= integer;i++) {
+        newNum = previousNum + currentNum
+        previousNum = fibonacciNumber
+        currentNum = newNum
+        console.log(currentNum)
+    }
+}
+fibbonaci(10);
+/*previous number plus current number equals to next number */
